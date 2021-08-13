b6acd6a51ab3d8d3216222412cac633b99a48232
Ross Thompson <rossthompson89@gmail.com>
2021-08-13 17:37:12 -0600

    updated license info

--- cluster_device_monitor.py
+++ cluster_device_monitor.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-# Copyright (c) 2021 Qumulo, Inc. All rights reserved.
+# Copyright (c) 2021 Qumulo, Inc.
 #
-# NOTICE: All information and intellectual property contained herein is the
-# confidential property of Qumulo, Inc. Reproduction or dissemination of the
-# information or intellectual property contained herein is strictly forbidden,
-# unless separate prior written permission has been obtained from Qumulo, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 cluster_device_monitor.py interacts with the Qumulo Rest API to retrieve the
